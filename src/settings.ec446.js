d63eeb2ec0dfb0a7bccf3f54e2d42077f3b2a285
VM <108159204+Vammclm@users.noreply.github.com>
2023-05-05 15:39:20 +0800

    Add files via upload

---
+++ src/settings.ec446.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/jiazai.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",main:"a2e8a"}};
