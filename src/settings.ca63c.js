e028af55159a8219adadcc147c518371d5b938de
VM <108159204+Vammclm@users.noreply.github.com>
2023-05-06 18:18:22 +0800

    Add files via upload

---
+++ src/settings.ca63c.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/02 - shouye/shouye - music.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",main:"19794"}};
