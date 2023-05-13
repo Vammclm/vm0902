b47f874ab5526f61435a9c1d1124b7b2e0867acd
VM <108159204+Vammclm@users.noreply.github.com>
2023-05-14 00:07:55 +0800

    Add files via upload

---
+++ src/settings.31e6a.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/02 - shouye/shouye.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",main:"15ab3"}};
